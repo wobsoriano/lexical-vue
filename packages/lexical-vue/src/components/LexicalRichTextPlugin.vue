763a8db0c03ac205735fcf4f7677999d5de4c253
Robert Soriano <sorianorobertc@gmail.com>
2022-04-15 12:37:45 -0700

    add rich text plugin

---
+++ packages/lexical-vue/src/components/LexicalRichTextPlugin.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { EditorState } from 'lexical'
+import { useCanShowPlaceholder } from '../composables/useCanShowPlaceholder'
+import { useEditor } from '../composables/useEditor'
+import { useRichTextSetup } from '../composables/useRichTextSetup'
+
+const props = defineProps<{
+  initialEditorState?: null | string | EditorState | (() => void)
+}>()
+
+const editor = useEditor()
+const showPlaceholder = useCanShowPlaceholder(editor)
+useRichTextSetup(editor, props.initialEditorState)
+</script>
+
+<template>
+  <slot name="contentEditable" />
+  <slot v-if="showPlaceholder" name="placeholder" />
+  <!-- <slot name="decorators"></slot> -->
+</template>
