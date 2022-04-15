7e5ce82f86087de951ae9ec6195e662c8dd5e91d
Robert Soriano <sorianorobertc@gmail.com>
2022-04-14 20:30:43 -0700

    update plain text example

--- packages/lexical-vue/src/components/index.ts
+++ packages/lexical-vue/src/components/index.ts
@@ -4,3 +4,9 @@ export {
 export {
   default as LexicalPlainTextPlugin,
 } from './LexicalPlainTextPlugin.vue'
+export {
+  default as LexicalComposer,
+} from './LexicalComposer.vue'
+export {
+  default as LexicalOnChangePlugin,
+} from './LexicalOnChangePlugin.vue'
