0b4a1b0066e9b8710acea52fba8dac0f5198e553
Robert Soriano <sorianorobertc@gmail.com>
2022-04-15 15:07:42 -0700

    update base url

--- examples/demo/vite.config.ts
+++ examples/demo/vite.config.ts
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/lexical-vue-demo.vercel.app/' : './',
 })
