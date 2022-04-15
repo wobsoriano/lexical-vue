05aba7814418a86c3884d19fe132704dea9923b8
Robert Soriano <sorianorobertc@gmail.com>
2022-04-15 14:46:55 -0700

    update undo/redo functions

--- examples/demo/src/components/ToolbarPlugin.vue
+++ examples/demo/src/components/ToolbarPlugin.vue
@@ -5,19 +5,44 @@ import {
   REDO_COMMAND,
   UNDO_COMMAND,
 } from 'lexical'
+import { mergeRegister } from '@lexical/utils'
 import { useEditor } from 'lexical-vue'
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
+
+const LowPriority = 1
 
 const toolbarRef = ref<HTMLDivElement>()
 const editor = useEditor()
+
+const canUndo = ref(false)
+const canRedo = ref(false)
+
+watchEffect(() => {
+  mergeRegister(editor.registerCommand(
+    CAN_UNDO_COMMAND,
+    (payload: boolean) => {
+      canUndo.value = payload
+      return false
+    },
+    LowPriority,
+  ),
+  editor.registerCommand(
+    CAN_REDO_COMMAND,
+    (payload: boolean) => {
+      canRedo.value = payload
+      return false
+    },
+    LowPriority,
+  ))
+})
 </script>
 
 <template>
   <div ref="toolbarRef" class="toolbar">
-    <button class="toolbar-item spaced" aria-label="Undo" @click="editor.dispatchCommand(UNDO_COMMAND)">
+    <button :disabled="!canUndo" class="toolbar-item spaced" aria-label="Undo" @click="editor.dispatchCommand(UNDO_COMMAND)">
       <i class="format undo" />
     </button>
-    <button class="toolbar-item spaced" aria-label="Redo" @click="editor.dispatchCommand(REDO_COMMAND)">
+    <button :disabled="!canRedo" class="toolbar-item spaced" aria-label="Redo" @click="editor.dispatchCommand(REDO_COMMAND)">
       <i class="format redo" />
     </button>
     <div class="divider" />
