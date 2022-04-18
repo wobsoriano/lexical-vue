869fd6ac24b0c90f00a993add032fec534ddf136
Robert Soriano <sorianorobertc@gmail.com>
2022-04-18 09:59:09 -0700

    refactor: @lexical/dragon now supports esm

--- packages/lexical-vue/src/composables/usePlainTextSetup.ts
+++ packages/lexical-vue/src/composables/usePlainTextSetup.ts
@@ -1,4 +1,4 @@
-import * as lexicalDragon from '@lexical/dragon'
+import { registerDragonSupport } from '@lexical/dragon'
 import { registerPlainText } from '@lexical/plain-text'
 import { mergeRegister } from '@lexical/utils'
 import type { EditorState, LexicalEditor } from 'lexical'
@@ -7,8 +7,7 @@ import { onUnmounted } from 'vue'
 export function usePlainTextSetup(editor: LexicalEditor, initialEditorState?: null | string | EditorState | (() => void)) {
   const unsub = mergeRegister(
     registerPlainText(editor, initialEditorState),
-    // @ts-expect-error: Lexical dragon esm when?
-    lexicalDragon.registerDragonSupport(editor),
+    registerDragonSupport(editor),
   )
 
   onUnmounted(() => {
--- packages/lexical-vue/src/composables/useRichTextSetup.ts
+++ packages/lexical-vue/src/composables/useRichTextSetup.ts
@@ -1,4 +1,4 @@
-import * as lexicalDragon from '@lexical/dragon'
+import { registerDragonSupport } from '@lexical/dragon'
 import { registerRichText } from '@lexical/rich-text'
 import { mergeRegister } from '@lexical/utils'
 import type { EditorState, LexicalEditor } from 'lexical'
@@ -7,8 +7,7 @@ import { onUnmounted } from 'vue'
 export function useRichTextSetup(editor: LexicalEditor, initialEditorState?: null | string | EditorState | (() => void)) {
   const unsub = mergeRegister(
     registerRichText(editor, initialEditorState),
-    // @ts-expect-error: Lexical dragon esm when?
-    lexicalDragon.registerDragonSupport(editor),
+    registerDragonSupport(editor),
   )
 
   onUnmounted(() => {
