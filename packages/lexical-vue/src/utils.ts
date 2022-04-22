aa05132f82a064194470020417814b6e1d6bbe0c
Robert Soriano <sorianorobertc@gmail.com>
2022-04-22 12:20:53 -0700

    refactor: add class helper source

--- packages/lexical-vue/src/utils.ts
+++ packages/lexical-vue/src/utils.ts
@@ -5,4 +5,7 @@ export function getRealValue<T>(item: Ref<T> | T) {
   return isRef(item) ? item.value : item
 }
 
+/**
+ * Taken from https://github.com/piotrwitek/utility-types/blob/master/src/utility-types.ts#L158
+ */
 export type Class<T> = new (...args: any[]) => T
