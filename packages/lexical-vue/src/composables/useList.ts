9827f151288292b97649cc1dac4acab9b161543a
Robert Soriano <sorianorobertc@gmail.com>
2022-05-01 12:08:30 -0700

    fix: useList types

--- packages/lexical-vue/src/composables/useList.ts
+++ packages/lexical-vue/src/composables/useList.ts
@@ -27,10 +27,7 @@ export function useList(editor: LexicalEditor): void {
       editor.registerCommand(
         INDENT_CONTENT_COMMAND,
         () => {
-          const hasHandledIndention = indentList()
-          if (hasHandledIndention)
-            return true
-
+          indentList()
           return false
         },
         COMMAND_PRIORITY_LOW,
@@ -38,10 +35,7 @@ export function useList(editor: LexicalEditor): void {
       editor.registerCommand(
         OUTDENT_CONTENT_COMMAND,
         () => {
-          const hasHandledIndention = outdentList()
-          if (hasHandledIndention)
-            return true
-
+          outdentList()
           return false
         },
         COMMAND_PRIORITY_LOW,
