b226cc4fed4c5c19c0725e767fbfbab0c4511198
Robert Soriano <sorianorobertc@gmail.com>
2022-06-03 12:16:19 -0700

    docs: upgrade to vitepress alpha

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,11 +7,14 @@ export default defineConfig({
   lastUpdated: true,
 
   themeConfig: {
-    repo: 'wobsoriano/lexical-vue',
-    docsDir: 'docs',
-    editLinks: true,
-    editLinkText: 'Edit this page on GitHub',
-    lastUpdated: 'Last Updated',
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/wobsoriano/lexical-vue' },
+    ],
+    // repo: 'wobsoriano/lexical-vue',
+    // docsDir: 'docs',
+    // editLinks: true,
+    // editLinkText: 'Edit this page on GitHub',
+    lastUpdatedText: 'Last Updated',
     nav: [
       { text: 'Guide', link: '/', activeMatch: '^/$|^/guide/' },
       { text: 'Playground', link: 'https://lexical-vue-playground.vercel.app' },
@@ -20,6 +23,9 @@ export default defineConfig({
       '/guide/': getGuideSidebar(),
       '/': getGuideSidebar(),
     },
+    footer: {
+      message: 'Released under the MIT License.',
+    },
   },
 })
 
@@ -27,14 +33,14 @@ function getGuideSidebar() {
   return [
     {
       text: 'Introduction',
-      children: [
+      items: [
         { text: 'What is Lexical Vue?', link: '/' },
         { text: 'Getting Started', link: '/guide/getting-started' },
       ],
     },
     {
       text: 'Plugins',
-      children: [
+      items: [
         { text: 'Available Plugins', link: '/plugins/available-plugins' },
         { text: 'Custom', link: '/plugins/custom' },
       ],
