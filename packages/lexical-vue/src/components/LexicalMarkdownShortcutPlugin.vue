26111e255f1592b0918c3bb5fb9fb3032adb626c
Robert Soriano <sorianorobertc@gmail.com>
2022-06-24 14:07:09 -0700

    chore(markdown): listen on mounted

--- packages/lexical-vue/src/components/LexicalMarkdownShortcutPlugin.vue
+++ packages/lexical-vue/src/components/LexicalMarkdownShortcutPlugin.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import { TRANSFORMERS, registerMarkdownShortcuts } from '@lexical/markdown'
 import type { Transformer } from '@lexical/markdown'
-import { defineProps, onUnmounted, watchEffect } from 'vue'
+import { defineProps, onMounted, onUnmounted } from 'vue'
 import { useEditor } from '../composables/useEditor'
 
 const props = withDefaults(defineProps<{
@@ -10,12 +10,11 @@ const props = withDefaults(defineProps<{
   transformers: () => [...TRANSFORMERS],
 })
 const editor = useEditor()
+
 let unregisterListener: () => void
-watchEffect((onInvalidate) => {
+
+onMounted(() => {
   unregisterListener = registerMarkdownShortcuts(editor, props.transformers)
-  onInvalidate(() => {
-    unregisterListener()
-  })
 })
 
 onUnmounted(() => {
