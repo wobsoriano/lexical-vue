a2d8fb4a027513f7e2ad621b2488d31642ddd0c5
wobsoriano <sorianorobertc@gmail.com>
2022-12-19 08:53:44 -0800

    feat(deps): bump lexical deps to 0.7.3

--- packages/lexical-vue/src/composables/useCanShowPlaceholder.ts
+++ packages/lexical-vue/src/composables/useCanShowPlaceholder.ts
@@ -5,14 +5,14 @@ import type { LexicalEditor } from 'lexical'
 export function useCanShowPlaceholder(editor: LexicalEditor) {
   const initialState = editor
     .getEditorState()
-    .read($canShowPlaceholderCurry(editor.isComposing(), editor.isEditable()))
+    .read($canShowPlaceholderCurry(editor.isComposing()))
 
   const canShowPlaceholder = ref(initialState)
 
   const unregisterListener = editor.registerUpdateListener(({ editorState }) => {
     const isComposing = editor.isComposing()
     canShowPlaceholder.value = editorState.read(
-      $canShowPlaceholderCurry(isComposing, editor.isEditable()),
+      $canShowPlaceholderCurry(isComposing),
     )
   })
 
