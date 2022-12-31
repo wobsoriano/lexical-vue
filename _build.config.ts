1d0812f2d904c85d796a06c148716b1954b613b3
wobsoriano <sorianorobertc@gmail.com>
2022-12-31 02:13:55 -0800

    build: migrate to tsup

--- _build.config.ts
+++ _build.config.ts
@@ -24,8 +24,8 @@ export default defineBuildConfig({
   entries: [
     'src/index',
   ],
-  declaration: false,
-  clean: false,
+  declaration: true,
+  clean: true,
   externals: [
     'lexical',
     'vue',
---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+import vuePlugin from 'esbuild-plugin-vue'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  splitting: false,
+  clean: true,
+  external: [/@lexical/],
+  dts: false,
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : `.${format}`,
+    }
+  },
+  esbuildPlugins: [vuePlugin()],
+})
