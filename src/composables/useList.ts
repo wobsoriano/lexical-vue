db18febd11b9b6d584b4f4a74ec2cd48ddb21447
wobsoriano <sorianorobertc@gmail.com>
2023-03-30 12:34:05 -0700

    fix: missing list imports

--- src/composables/useList.ts
+++ src/composables/useList.ts
@@ -5,17 +5,13 @@ import {
   INSERT_ORDERED_LIST_COMMAND,
   INSERT_UNORDERED_LIST_COMMAND,
   REMOVE_LIST_COMMAND,
-  indentList,
   insertList,
-  outdentList,
   removeList,
 } from '@lexical/list'
 import { mergeRegister } from '@lexical/utils'
 import {
   COMMAND_PRIORITY_LOW,
-  INDENT_CONTENT_COMMAND,
   INSERT_PARAGRAPH_COMMAND,
-  OUTDENT_CONTENT_COMMAND,
 } from 'lexical'
 import { useMounted } from './useMounted'
 
@@ -23,56 +19,42 @@ export function useList(editor: LexicalEditor) {
   useMounted(() => {
     return mergeRegister(
       editor.registerCommand(
-        INDENT_CONTENT_COMMAND,
-        () => {
-          indentList()
-          return false
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
-        OUTDENT_CONTENT_COMMAND,
-        () => {
-          outdentList()
-          return false
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
         INSERT_ORDERED_LIST_COMMAND,
         () => {
-          insertList(editor, 'number')
-          return true
+          insertList(editor, 'number');
+          return true;
         },
         COMMAND_PRIORITY_LOW,
       ),
       editor.registerCommand(
         INSERT_UNORDERED_LIST_COMMAND,
         () => {
-          insertList(editor, 'bullet')
-          return true
+          insertList(editor, 'bullet');
+          return true;
         },
         COMMAND_PRIORITY_LOW,
       ),
       editor.registerCommand(
         REMOVE_LIST_COMMAND,
         () => {
-          removeList(editor)
-          return true
+          removeList(editor);
+          return true;
         },
         COMMAND_PRIORITY_LOW,
       ),
       editor.registerCommand(
         INSERT_PARAGRAPH_COMMAND,
         () => {
-          const hasHandledInsertParagraph = $handleListInsertParagraph()
-          if (hasHandledInsertParagraph)
-            return true
+          const hasHandledInsertParagraph = $handleListInsertParagraph();
 
-          return false
+          if (hasHandledInsertParagraph) {
+            return true;
+          }
+
+          return false;
         },
         COMMAND_PRIORITY_LOW,
       ),
-    )
+    );
   })
 }
