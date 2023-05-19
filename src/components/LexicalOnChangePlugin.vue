d3bcd7a8fceacdd1dc6e798c61ac676476a1f7c5
wobsoriano <sorianorobertc@gmail.com>
2023-05-18 20:27:25 -0700

    refactor: remove two-way binding of OnChange plugin

--- src/components/LexicalOnChangePlugin.vue
+++ src/components/LexicalOnChangePlugin.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import type { EditorState, LexicalEditor } from 'lexical'
-import { $getRoot } from 'lexical'
 import { useEditor } from '../composables'
 import { useMounted } from '../composables/useMounted'
 
 const props = withDefaults(defineProps<{
   ignoreInitialChange?: boolean
   ignoreSelectionChange?: boolean
-  modelValue?: string
 }>(), {
   ignoreInitialChange: true,
   ignoreSelectionChange: false,
@@ -15,12 +13,9 @@ const props = withDefaults(defineProps<{
 
 const emit = defineEmits<{
   (e: 'change', editorState: EditorState, editor: LexicalEditor): void
-  (e: 'update:modelValue', payload: string): void
 }>()
 
 const editor = useEditor()
-
-const getRoot = $getRoot
 
 useMounted(() => {
   return editor.registerUpdateListener(({ editorState, dirtyElements, dirtyLeaves, prevEditorState }) => {
@@ -35,10 +30,6 @@ useMounted(() => {
       return
 
     emit('change', editorState, editor)
-
-    editorState.read(() => {
-      emit('update:modelValue', getRoot().getTextContent())
-    })
   })
 })
 </script>
