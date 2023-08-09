5852e7c25443bdb3add1820850e46153705a647c
Ivan Zvonar <ivan@zvonar.dev>
2023-08-09 23:48:29 +0200

    initial conversion

--- playground/src/components/Editor.vue
+++ playground/src/components/Editor.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import type { CreateEditorArgs } from 'lexical'
+import type { CreateEditorArgs, LexicalEditor } from 'lexical'
 import { $createParagraphNode, $createTextNode, $getRoot } from 'lexical'
 import {
   LexicalAutoFocusPlugin,
+  LexicalCollaborationPlugin,
   LexicalComposer,
   LexicalContentEditable,
   LexicalHashtagPlugin,
@@ -11,11 +12,13 @@ import {
   LexicalListPlugin,
   LexicalRichTextPlugin,
 } from 'lexical-vue'
-import { $createHeadingNode, $createQuoteNode, HeadingNode, QuoteNode } from '@lexical/rich-text'
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+import { HeadingNode, QuoteNode } from '@lexical/rich-text'
 import { TableCellNode, TableNode, TableRowNode } from '@lexical/table'
-import { $createListItemNode, $createListNode, ListItemNode, ListNode } from '@lexical/list'
+import { ListItemNode, ListNode } from '@lexical/list'
 import { CodeHighlightNode, CodeNode } from '@lexical/code'
-import { $createLinkNode, AutoLinkNode, LinkNode } from '@lexical/link'
+import { AutoLinkNode, LinkNode } from '@lexical/link'
 import { HashtagNode } from '@lexical/hashtag'
 
 import exampleTheme from '../themes/example'
@@ -28,86 +31,12 @@ import MarkdownShortcutPlugin from './MarkdownShortcutPlugin.vue'
 import EmojisPlugin from './EmojisPlugin.vue'
 import { EmojiNode } from './EmojiNode'
 
-function prePopulatedRichText() {
+function initialEditorState(editor: LexicalEditor): void {
   const root = $getRoot()
-  if (root.getFirstChild() === null) {
-    const heading = $createHeadingNode('h1')
-    heading.append($createTextNode('Welcome to the playground'))
-    root.append(heading)
-    const quote = $createQuoteNode()
-    quote.append(
-      $createTextNode(
-        'In case you were wondering what the black box at the bottom is – it\'s the debug view, showing the current state of editor. '
-          + 'You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.',
-      ),
-    )
-    root.append(quote)
-    const paragraph = $createParagraphNode()
-    paragraph.append(
-      $createTextNode('The playground is a demo environment built with '),
-      $createTextNode('lexical').toggleFormat('code'),
-      $createTextNode(' and '),
-      $createTextNode('lexical-vue').toggleFormat('code'),
-      $createTextNode('.'),
-      $createTextNode(' Try typing in '),
-      $createTextNode('some text').toggleFormat('bold'),
-      $createTextNode(' with '),
-      $createTextNode('different').toggleFormat('italic'),
-      $createTextNode(' formats.'),
-    )
-    root.append(paragraph)
-    const paragraph2 = $createParagraphNode()
-    paragraph2.append(
-      $createTextNode(
-        'Make sure to check out the various plugins in the toolbar. You can also use #hashtags or @-mentions too!',
-      ),
-    )
-    root.append(paragraph2)
-    const paragraph3 = $createParagraphNode()
-    paragraph3.append(
-      $createTextNode('If you\'d like to find out more about Lexical, you can:'),
-    )
-    root.append(paragraph3)
-    const list = $createListNode('bullet')
-    list.append(
-      $createListItemNode().append(
-        $createTextNode('Visit the '),
-        $createLinkNode('https://lexical.dev/').append(
-          $createTextNode('Lexical website'),
-        ),
-        $createTextNode(' for documentation and more information.'),
-      ),
-      $createListItemNode().append(
-        $createTextNode('Check out the code on the '),
-        $createLinkNode('https://github.com/wobsoriano/lexical-vue').append(
-          $createTextNode('lexical-vue GitHub repository'),
-        ),
-        $createTextNode('.'),
-      ),
-      $createListItemNode().append(
-        $createTextNode('Playground code can be found '),
-        $createLinkNode(
-          'https://github.com/wobsoriano/lexical-vue/tree/main/playground',
-        ).append($createTextNode('here')),
-        $createTextNode('.'),
-      ),
-      $createListItemNode().append(
-        $createTextNode('Join the '),
-        $createLinkNode('https://discord.com/invite/KmG4wQnnD9').append(
-          $createTextNode('Lexical Discord Server'),
-        ),
-        $createTextNode(' and chat with the team.'),
-      ),
-    )
-    root.append(list)
-    const paragraph4 = $createParagraphNode()
-    paragraph4.append(
-      $createTextNode(
-        'Lastly, we\'re constantly adding cool new features to this playground. So make sure you check back here when you next get a chance :).',
-      ),
-    )
-    root.append(paragraph4)
-  }
+  const paragraph = $createParagraphNode()
+  const text = $createTextNode('Welcome to collab!')
+  paragraph.append(text)
+  root.append(paragraph)
 }
 
 const config: CreateEditorArgs = {
@@ -127,8 +56,9 @@ const config: CreateEditorArgs = {
     HashtagNode,
     EmojiNode,
   ],
+  namespace: 'Demo',
   editable: true,
-  editorState: prePopulatedRichText as any,
+  editorState: initialEditorState as any,
 }
 
 function onError(error: Error) {
@@ -162,6 +92,23 @@ function onError(error: Error) {
         <LexicalHashtagPlugin />
         <MarkdownShortcutPlugin />
         <EmojisPlugin />
+        <LexicalCollaborationPlugin
+          id="document-id"
+          :provider-factory="(id: string, yjsDocMap: any) => {
+            const doc = new Y.Doc({ autoLoad: true });
+            yjsDocMap.set(id, doc);
+            console.log(id, doc.getText())
+            const provider = new WebsocketProvider(
+              'ws://localhost:1234',
+              id,
+              doc,
+            );
+
+            return provider;
+          }"
+          :initial-editor-state="initialEditorState"
+          :should-bootstrap="false"
+        />
       </div>
     </div>
   </LexicalComposer>
---
+++ src/components/LexicalCollaborationPlugin.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import type { Doc } from 'yjs'
+
+import type { ExcludedProperties, Provider } from '@lexical/yjs'
+import { computed, watch, watchEffect } from 'vue'
+import type { CursorsContainerRef } from '../composables'
+import {
+  useEditor,
+  useEffect,
+  useYjsCollaboration,
+  useYjsFocusTracking,
+  useYjsHistory,
+} from '../composables'
+import type { InitialEditorStateType } from '../types'
+import collaborationContext from '../composables/useCollaborationContext'
+
+const props = defineProps<{
+  id: string
+  providerFactory: (id: string, yjsDocMap: Map<string, Doc>) => Provider
+  shouldBootstrap: boolean
+  username?: string
+  cursorColor?: string
+  cursorsContainerRef?: CursorsContainerRef
+  initialEditorState?: InitialEditorStateType
+  excludedProperties?: ExcludedProperties
+  // `awarenessData` parameter allows arbitrary data to be added to the awareness.
+  awarenessData?: object
+}>()
+
+watchEffect(() => {
+  if (props.username !== undefined)
+    collaborationContext.value.name = props.username
+  if (props.cursorColor !== undefined)
+    collaborationContext.value.color = props.cursorColor
+})
+
+const editor = useEditor()
+
+useEffect(() => {
+  collaborationContext.value.isCollabActive = true
+
+  return () => {
+    // Reseting flag only when unmount top level editor collab plugin. Nested
+    // editors (e.g. image caption) should unmount without affecting it
+    if (editor._parentEditor == null)
+      collaborationContext.value.isCollabActive = false
+  }
+})
+
+const provider = computed(() =>
+  props.providerFactory(props.id, collaborationContext.value.yjsDocMap),
+)
+
+const [cursors, binding] = useYjsCollaboration(
+  editor,
+  props.id,
+  provider.value,
+  collaborationContext.value.yjsDocMap,
+  collaborationContext.value.name,
+  collaborationContext.value.color,
+  props.shouldBootstrap,
+  props.cursorsContainerRef,
+  props.initialEditorState,
+  props.excludedProperties,
+  props.awarenessData,
+)
+
+watch(
+  () => binding,
+  (b) => {
+    collaborationContext.value.clientID = b.clientID
+  },
+)
+
+useYjsHistory(editor, binding)
+useYjsFocusTracking(
+  editor,
+  provider.value,
+  collaborationContext.value.name,
+  collaborationContext.value.color,
+  props.awarenessData,
+)
+
+defineExpose({
+  cursors,
+})
+</script>
