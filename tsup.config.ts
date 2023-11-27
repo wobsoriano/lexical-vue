81490ba9f6a306c12154616d610b92db52f90d91
aquarius-wing <aquarius_wing@icloud.com>
2023-11-27 17:32:24 +0800

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   dts: false,
   outExtension({ format }) {
     return {
-      js: format === 'esm' ? '.mjs' : '.js',
+      js: format === 'esm' ? '.mjs' : '.cjs',
     }
   },
   esbuildPlugins: [vuePlugin()],
