205a0eca0180c3a467e92f97a2cb246c2c719123
wobsoriano <sorianorobertc@gmail.com>
2025-02-23 21:13:39 -0800

    chore: remove copyright

--- src/components/LexicalCheckListPlugin.vue
+++ src/components/LexicalCheckListPlugin.vue
@@ -1,13 +1,4 @@
 <script setup lang="ts">
-/**
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow strict
- */
-
 import type { ListItemNode } from '@lexical/list'
 import {
   $getNearestNodeFromDOMNode,
