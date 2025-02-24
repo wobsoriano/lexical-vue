5ddab5a084b302724283e6a24d5a59ac1f932f34
wobsoriano <sorianorobertc@gmail.com>
2025-02-23 23:23:56 -0800

    chore: Update LexicalListPlugin

--- src/components/LexicalListPlugin.vue
+++ src/components/LexicalListPlugin.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import { ListItemNode, ListNode } from '@lexical/list'
+import { onMounted } from 'vue'
 import { useLexicalComposer, useList } from '../composables'
 
 const editor = useLexicalComposer()
+onMounted(() => {
+  if (!editor.hasNodes([ListNode, ListItemNode])) {
+    throw new Error(
+      'ListPlugin: ListNode and/or ListItemNode not registered on editor',
+    )
+  }
+})
+
 useList(editor)
 </script>
 
