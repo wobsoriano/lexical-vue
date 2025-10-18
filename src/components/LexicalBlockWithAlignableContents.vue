bad11204effd4cd4c8c063d4fe2e47b3cddaa228
wobsoriano <sorianorobertc@gmail.com>
2025-10-18 13:38:11 -0700

    chore: update LexicalBlockWithAlignableContents

--- src/components/LexicalBlockWithAlignableContents.vue
+++ src/components/LexicalBlockWithAlignableContents.vue
@@ -16,9 +16,8 @@ import {
   COMMAND_PRIORITY_LOW,
   FORMAT_ELEMENT_COMMAND,
 } from 'lexical'
-import { useTemplateRef } from 'vue'
+import { useTemplateRef, watchEffect } from 'vue'
 import { useLexicalComposer, useLexicalNodeSelection } from '../composables'
-import { useMounted } from '../composables/useMounted'
 import { $isDecoratorBlockNode } from './LexicalDecoratorBlockNode'
 
 const props = defineProps<{
@@ -32,8 +31,8 @@ const editor = useLexicalComposer()
 const { isSelected, setSelected, clearSelection } = useLexicalNodeSelection(() => props.nodeKey)
 const containerRef = useTemplateRef('containerRef')
 
-useMounted(() => {
-  return mergeRegister(
+watchEffect((onInvalidate) => {
+  const unregister = mergeRegister(
     editor.registerCommand<ElementFormatType>(
       FORMAT_ELEMENT_COMMAND,
       (formatType) => {
@@ -82,6 +81,8 @@ useMounted(() => {
       COMMAND_PRIORITY_LOW,
     ),
   )
+
+  onInvalidate(unregister)
 })
 </script>
 
